8672635bbb26e9dbc53b57bd0e53ba2839613306
Piotr Dubiela <piotrd.tnt@gmail.com>
2024-05-28 09:21:25 +0200

    added spotless

--- src/test/java/po/test/DivisionTest.java
+++ src/test/java/po/test/DivisionTest.java
@@ -5,21 +5,21 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import po.CalculatorPage;
 
-public class DivisionTest extends BaseTest{
+public class DivisionTest extends BaseTest {
     @DataProvider(name = "Numbers")
-    public Object[][] dataProvider(){
-        return new Object[][]{
-                {"0", "100", "0"},
-                {"60","3","20"},
-                {"5","0","Not a Number"},
-                {"20", "100", "0.2"}
+    public Object[][] dataProvider() {
+        return new Object[][] {
+            {"0", "100", "0"},
+            {"60", "3", "20"},
+            {"5", "0", "Not a Number"},
+            {"20", "100", "0.2"}
         };
     }
 
     @Test(dataProvider = "Numbers")
-    public void divisionTest(String a, String b, String expectedResult){
+    public void divisionTest(String a, String b, String expectedResult) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        String result = calculatorPage.divide(a,b);
+        String result = calculatorPage.divide(a, b);
         Assert.assertEquals(result, expectedResult);
     }
 }
